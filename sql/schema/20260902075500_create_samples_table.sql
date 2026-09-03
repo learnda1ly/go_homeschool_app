1e499a37563b00f51666e42890003f13e07ded2d
learnda1ly <stephen.a.quinlan@gmail.com>
2026-09-02 20:05:39 -0500

    added sql migration for homework samples table

---
+++ sql/schema/20260902075500_create_samples_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE samples(
+  id SERIAL PRIMARY KEY,
+  learner_id INTEGER NOT NULL REFERENCES learners(id) ON DELETE CASCADE,
+  subject_id INTEGER NOT NULL REFERENCES subjects(id) ON DELETE CASCADE,
+  title VARCHAR(250) NOT NULL,
+  description VARCHAR(2500),
+  notes varchar(2500),
+  url VARCHAR(250),
+  letter_grade VARCHAR(5),
+  percentage_grade int,
+  completion_time TIMESTAMP,
+  scheduled_time TIMESTAMP,
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS samples;
